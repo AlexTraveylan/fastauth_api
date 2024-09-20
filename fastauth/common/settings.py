59ac5ec6a1aba33d1846e2b892e055f2b3cf133d
AlexTraveylan <monsieurdemares@gmail.com>
2024-09-21 00:02:29 +0200

    feature: update settings to include algorithm, secret key, and env file support in FastAuth configuration

--- fastauth/common/settings.py
+++ fastauth/common/settings.py
@@ -3,10 +3,17 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    AES_KEY: str
+    AES_KEY: bytes
+    ALGORITHM: str
+    SECRET_KEY: str
     EMAIL: str
     PRODUCTION: bool
     DEBUG: bool
 
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 SETTINGS = Settings()
+pass
