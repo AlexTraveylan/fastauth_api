3fc1a8b4f1de85769de2a746b6013c08b76d13d6
AlexTraveylan <monsieurdemares@gmail.com>
2024-09-23 13:37:23 +0200

    feat: add logging configuration in __init__.py files for enhanced debugging and monitoring capabilities in fastauth module

--- fastauth/__init__.py
+++ fastauth/__init__.py
@@ -1,3 +1,7 @@
+import logging
+
 from fastauth.api_key.models import *  # noqa: F403
 from fastauth.user.models import *  # noqa: F403
 from fastauth.user_email.models import *  # noqa: F403
+
+logging.basicConfig(level=logging.INFO)
--- fastauth/database/__init__.py
+++ fastauth/database/__init__.py
@@ -1,8 +1,3 @@
-import logging
-
 from fastauth.database.engine import create_db_and_tables, get_engine
-
-logging.basicConfig(level=logging.INFO)
-
 
 create_db_and_tables(engine=get_engine())
